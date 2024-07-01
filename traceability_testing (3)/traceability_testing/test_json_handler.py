235b38504d29c5676fa691dd66ea718544b9191a
BidhyashreeDahal <bidhyashree.dahal@dcmail.ca>
2024-07-01 19:56:57 -0400

    Update
    Made some changes to pass the pytest.

--- traceability_testing (3)/traceability_testing/test_json_handler.py
+++ traceability_testing (3)/traceability_testing/test_json_handler.py
@@ -4,6 +4,7 @@ This module contains unit tests for JsonHandler class.
 """
 import pytest
 from json_handler import JsonHandler
+
 def requirement(ides):
     """Decorator to assign requirement ID to test functions."""
     def decorator(function):
@@ -21,34 +22,33 @@ def temp_file_path(tmp_path):
     '''Fixture to provide a temporary file path.'''
     return tmp_path / "test.json"
 
-
 @requirement("REQ-101")
-def test_read_json(json_handler, temp_file):
+def test_read_json(json_handler_instance, temp_file_path):
     '''Test reading JSON data from a file using JsonHandler.'''
     data = {"test": "data"}
-    json_handler.write_json(data, temp_file)
-    read_data = json_handler.read_json(temp_file)
+    json_handler_instance.write_json(data, temp_file_path)
+    read_data = json_handler_instance.read_json(temp_file_path)
     assert read_data == data
 
 @requirement("REQ-102")
-def test_write_json(json_handler, temp_file):
+def test_write_json(json_handler_instance, temp_file_path):
     '''Test writing and reading JSON data using JsonHandler.'''
     data = {"test": "data"}
-    json_handler.write_json(data, temp_file)
-    read_data = json_handler.read_json(temp_file)
+    json_handler_instance.write_json(data, temp_file_path)
+    read_data = json_handler_instance.read_json(temp_file_path)
     assert read_data == data
 
 @requirement("REQ-103")
-def test_check_key(json_handler):
+def test_check_key(json_handler_instance):
     '''Test checking for key in JSON data using JsonHandler.'''
     data = {"test": "data"}
-    assert json_handler.check_key(data, 'test')
+    assert json_handler_instance.check_key(data, 'test')
 
 @requirement("REQ-104")
-def test_update_json(json_handler, temp_file):
+def test_update_json(json_handler_instance, temp_file_path):
     '''Test updating JSON data using JsonHandler.'''
     data = {"test": "data"}
-    json_handler.write_json(data, temp_file)
-    json_handler.update_json("test", "new data", temp_file)
-    updated_data = json_handler.read_json(temp_file)
+    json_handler_instance.write_json(data, temp_file_path)
+    json_handler_instance.update_json("test", "new data", temp_file_path)
+    updated_data = json_handler_instance.read_json(temp_file_path)
     assert updated_data["test"] == "new data"
